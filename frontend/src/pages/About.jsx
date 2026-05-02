60e7e1d70123dd92f5e40affbae394b5cbfc674b
Ralph <ralph.tolentino_ariza0007@stud.hkr.se>
2026-05-02 06:41:56 +0200

    Fixed format

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -8,30 +8,37 @@ function About() {
 
       <h4>Team Members</h4>
       <ul>
-        <li>Mhd Osama Alsaheb     — Focus area: placeholder</li>
-        <li>Ted J. Burgess        — Focus area: placeholder</li>
+        <li>Mhd Osama Alsaheb — Focus area: placeholder</li>
+        <li>Ted J. Burgess — Focus area: placeholder</li>
         <li>Ralph Tolentino Ariza — Focus area: placeholder</li>
-        <li>Osayi Uwadiae         — Focus area: placeholder</li>
+        <li>Osayi Uwadiae — Focus area: placeholder</li>
       </ul>
 
       <h3>App Purpose</h3>
       <p>
-        MoveUp is a fullstack web application aimed at helping people build better 
-        habits when they spend a lot of time at their computers. The idea is to remind users 
-        to take short breaks at regular intervals and encourage them to actually get up and move around.
-        When users confirm that they have taken a break, they can earn points, build streaks, and see how 
-        they compare with others on a leaderboard. The goal is to make something that is not only useful,
-        but also a bit motivating and fun to use. Overall, the project focuses on a very common issue 
-        which is people sitting for long periods without moving while studying or working.
+        MoveUp is a fullstack web application aimed at helping people build
+        better habits when they spend a lot of time at their computers. The idea
+        is to remind users to take short breaks at regular intervals and
+        encourage them to actually get up and move around. When users confirm
+        that they have taken a break, they can earn points, build streaks, and
+        see how they compare with others on a leaderboard. The goal is to make
+        something that is not only useful, but also a bit motivating and fun to
+        use. Overall, the project focuses on a very common issue which is people
+        sitting for long periods without moving while studying or working.
       </p>
 
       <h3>Planned Core Features</h3>
       <ul>
         <li>Users can create accounts, login with existing accounts</li>
-        <li>View account statistics: Completed breaks, streaks, leaderboard position</li>
+        <li>
+          View account statistics: Completed breaks, streaks, leaderboard
+          position
+        </li>
         <li>Receive reminders to take a break</li>
         <li>Attain points through movement</li>
-        <li>A leaderboard. A user's total points predicts leaderboard position</li>
+        <li>
+          A leaderboard. A user's total points predicts leaderboard position
+        </li>
       </ul>
     </section>
   );
