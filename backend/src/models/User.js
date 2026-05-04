d6bf30332cc850c8814a9cc9466a20e9a7d10dc3
Osama Alsaheb <sahebosama12@gmail.com>
2026-05-04 17:15:56 +0200

    feat: add User model

---
+++ backend/src/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    id: {
+      type: Number,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    points: {
+      type: Number,
+      default: 0,
+    },
+    rank: {
+      type: Number,
+      required: true,
+      trim: true,
+      unique: true,
+    }
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("User", userSchema);
