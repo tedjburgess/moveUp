abb5b8de9fdcd0210548116e329ca96959b0998f
Ralph <ralph.tolentino_ariza0007@stud.hkr.se>
2026-05-05 08:23:53 +0200

    Edited Homepage and added basic reminder and signup page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
-function Home() {
+function Home({ setCurrentPage }) {
   return (
     <section>
+      <button onClick={() => setCurrentPage("signup")}>
+        Sign Up Now!
+      </button>
+
       <h2>Home</h2>
-      <p>Welcome to Movement Reminder.</p>
-      <p>
-        Planned features: movement reminders, activity tracking, and simple
-        wellness tips.
-      </p>
+      <p>Welcome to MoveUp!</p>
+
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi porro vitae labore corrupti, consequuntur ex delectus. Itaque odit, numquam asperiores, dolore ipsum amet sint tempore quisquam esse qui quibusdam deserunt!</p>
     </section>
   );
 }
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,16 @@
+function SignUp() {
+  return (
+    <section>
+      <h2>Sign Up</h2>
+
+      <input type="text" placeholder="Username" />
+      <br /><br />
+      <input type="password" placeholder="Password" />
+      <br /><br />
+
+      <button>Create Account</button>
+    </section>
+  );
+}
+
+export default SignUp;
